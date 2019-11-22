38fb1e364bdff5de495fb898f164ef28ef09e908
Molly Moran <40001570+mgmoran@users.noreply.github.com>
2019-11-22 08:53:52 -0500

    cleaner commenting

--- train_test_tagger.py
+++ train_test_tagger.py
@@ -19,10 +19,7 @@ def read_in_plain_data(filename):
     return sents
 
 def output_auto_data(auto_data,filename='auto_dev.tagged'):
-    ''' According to the data structure you used for "auto_data",
-        write code here to output your auto tagged data into a file,
-        using the same format as the provided gold data (i.e. word_pos word_pos ...). 
-    '''
+    """ Outputs tagged data into a file, which can be read in by the scorer to calculate p-r-f. """
     print("outputting tagged data into file...")
     with open(filename,'w+') as f:
         for sent in auto_data:
@@ -30,6 +27,7 @@ def output_auto_data(auto_data,filename='auto_dev.tagged'):
             f.write(" ".join(sent) + "\n")
 
 if __name__ == '__main__':
+    ### Extracting vocab/featurizing data
     extract_vocabulary("train/ptb_02-21.tagged")
     gold_dev_data = read_in_gold_data("dev/ptb_22.tagged")
     train_data = read_in_gold_data("train/ptb_02-21.tagged")
@@ -38,16 +36,15 @@ if __name__ == '__main__':
     plain_dev_file = 'dev/ptb_22.snt'
     plain_dev_data = read_in_plain_data(plain_dev_file)
 
-    # # # Train your tagger
+    ### Training tagger
     my_tagger = Perceptron_POS_Tagger()
     my_tagger.train(train_data, 'dev/ptb_22.tagged', plain_dev_data)
 
-    # # # Apply your tagger on dev & test data
+    ### Tagging dev and test data
     auto_dev_data = my_tagger.tag(plain_dev_data)
     auto_test_data = my_tagger.tag(test_data)
 
-    # # # Output your auto tagged data
-
+    ### Outputting readable tagged data
     output_auto_data(auto_dev_data)
     output_auto_data(auto_test_data, 'auto_test.tagged')
 
